3208e20adec660848828e2bdd5b14568615b34de
naxsyambis <naxsyambis@gmail.com>
2025-11-28 16:05:40 +0700

    Menambahkan Data Access Object (DAO) untuk entitas Siswa. DAO ini menyediakan: - `getAllSiswa()`: Mengambil semua siswa sebagai Flow (diurutkan berdasarkan nama). - `insert(siswa: Siswa)`: Menyimpan data siswa baru (mengabaikan konflik).

---
+++ app/src/main/java/com/example/a163_roomdatabase/room/SiswaDao.kt
@@ -0,0 +1,16 @@
+package com.example.a163_roomdatabase.room
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface SiswaDao {
+    @Query("SELECT * FROM tblSiswa ORDER BY nama ASC")
+    fun getAllSiswa(): Flow<List<Siswa>>
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insert(siswa: Siswa)
+}
