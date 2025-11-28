98fb1b1df45ea865f58b54246299ee9a31c1b3b6
naxsyambis <naxsyambis@gmail.com>
2025-11-28 16:10:14 +0700

    Mengatur komponen aplikasi untuk menyediakan dependensi (RepositoriSiswa). - Mendefinisikan interface ContainerApp. - Membuat ContainerDataApp untuk inisialisasi lazy RepositoriSiswa dan DAO. - Mengimplementasikan AplikasiSiswa untuk menyimpan container dan inisialisasi saat aplikasi dimulai.

---
+++ app/src/main/java/com/example/a163_roomdatabase/repositori/ContainerApp.kt
@@ -0,0 +1,26 @@
+package com.example.a163_roomdatabase.repositori
+
+import android.app.Application
+import android.content.Context
+import com.example.a163_roomdatabase.room.DatabaseSiswa
+
+interface ContainerApp {
+    val repositoriSiswa : RepositoriSiswa
+}
+
+class ContainerDataApp(private val context: Context):
+        ContainerApp {
+            override val repositoriSiswa: RepositoriSiswa by lazy {
+                OfflineRepositoriSiswa(
+                    DatabaseSiswa.getDatabase(context).siswaDao())
+            }
+        }
+
+class AplikasiSiswa : Application() {
+    lateinit var container: ContainerApp
+
+    override fun onCreate() {
+        super.onCreate()
+        container = ContainerDataApp(this)
+    }
+}
