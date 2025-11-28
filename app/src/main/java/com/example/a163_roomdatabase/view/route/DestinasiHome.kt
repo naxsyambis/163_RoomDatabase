fcff5a073f879be52bfe277a80a38fb19c7a05d6
naxsyambis <naxsyambis@gmail.com>
2025-11-28 20:03:31 +0700

    Tambahkan objek DestinasiHome
    Mendefinisikan tujuan navigasi untuk layar utama aplikasi (Home Screen).
    - Route: "home".
    - Title Resource: R.string.app_name.

---
+++ app/src/main/java/com/example/a163_roomdatabase/view/route/DestinasiHome.kt
@@ -0,0 +1,8 @@
+package com.example.a163_roomdatabase.view.route
+
+import com.example.a163_roomdatabase.R
+
+object DestinasiHome : DestinasiNavigasi {
+    override val route = "home"
+    override val titleRes = R.string.app_name
+}
