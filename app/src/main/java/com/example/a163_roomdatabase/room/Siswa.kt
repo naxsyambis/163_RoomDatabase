6ef43dd7977c5fe2c8927b8f4bd356d39a3752bf
naxsyambis <naxsyambis@gmail.com>
2025-11-28 15:20:04 +0700

    Menambahkan data class siswa pada class data Siswa.kt

--- app/src/main/java/com/example/a163_roomdatabase/room/Siswa.kt
+++ app/src/main/java/com/example/a163_roomdatabase/room/Siswa.kt
@@ -1,3 +1,13 @@
 package com.example.a163_roomdatabase.room
 
-data class Siswa()
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "tblSiswa")
+data class Siswa(
+    @PrimaryKey(autoGenerate = true)
+    val id : Int = 0,
+    val nama : String,
+    val alamat : String,
+    val telpon : String
+)
