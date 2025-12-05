5dff491b4a12e14ffd68451da7795918b0be43a9
naxsyambis <naxsyambis@gmail.com>
2025-12-05 20:55:15 +0700

    memperbaiki eror

--- app/src/main/java/com/example/a163_roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/a163_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -10,20 +10,18 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
 import com.example.a163_roomdatabase.view.DetailSiswaScreen
+import com.example.a163_roomdatabase.view.EditSiswaScreen
 import com.example.a163_roomdatabase.view.EntrySiswaScreen
 import com.example.a163_roomdatabase.view.HomeScreen
 import com.example.a163_roomdatabase.view.route.DestinasiDetailSiswa
 import com.example.a163_roomdatabase.view.route.DestinasiDetailSiswa.itemIdArg
+import com.example.a163_roomdatabase.view.route.DestinasiEditSiswa
 import com.example.a163_roomdatabase.view.route.DestinasiEntry
 import com.example.a163_roomdatabase.view.route.DestinasiHome
 
-
 @Composable
-fun SiswaApp(
-    navController: NavHostController = rememberNavController(),
-    modifier: Modifier = Modifier
-) {
-    HostNavigasi(navController = navController, modifier = modifier)
+fun SiswaApp(navController: NavHostController= rememberNavController(), modifier: Modifier){
+    HostNavigasi(navController = navController)
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -31,39 +29,49 @@ fun SiswaApp(
 fun HostNavigasi(
     navController: NavHostController,
     modifier: Modifier = Modifier
-) {
-    NavHost(
-        navController = navController,
-        startDestination = DestinasiHome.route,
-        modifier = modifier
-    ) {
+){
+    NavHost(navController=navController, startDestination = DestinasiHome.route, modifier = Modifier)
+    {
         composable(DestinasiHome.route) {
             HomeScreen(
-                navigateToItemEntry = {
-                    navController.navigate(DestinasiEntry.route)
-                },
-
-                navigateToItemUpdate = { id ->
-                    navController.navigate("${DestinasiDetailSiswa.route}/$id")
-                }
-            )
-        }
-
-        composable(DestinasiEntry.route) {
-            EntrySiswaScreen(
+                navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
+                navigateToItemUpdate = { navController.navigate("${DestinasiDetailSiswa.route}/${it}") },
                 navigateBack = { navController.popBackStack() }
             )
         }
+        composable(DestinasiEntry.route) {
+            EntrySiswaScreen(navigateBack = { navController.popBackStack() })
+        }
 
-        composable(route = DestinasiDetailSiswa.routeWithArgs,
-            arguments = listOf(navArgument(itemIdArg) {
+        composable(
+            route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument(DestinasiDetailSiswa.itemIdArg) {
                 type = NavType.IntType
             })
-        ){
+        ) {
             DetailSiswaScreen(
-                navigateBack = {navController.navigateUp()}
+
+                navigateToEditItem = { navController.navigate("${DestinasiEditSiswa.route}/$it") },
+
+                navigateBack = { navController.navigateUp() }
             )
         }
 
+        composable(
+            route = DestinasiEditSiswa.routeWithArgs,
+            arguments = listOf(
+                navArgument(itemIdArg) {
+                    type = NavType.IntType
+                    defaultValue = -1 // Pilihan: Menambahkan defaultValue untuk keamanan
+                }
+            )
+        ) { backStackEntry -> // backStackEntry diperlukan jika Anda ingin mengakses argumen secara langsung di sini
+            EditSiswaScreen(
+                navigateBack = { navController.popBackStack() },
+                onNavigateUp = { navController.navigateUp() }
+                // Anda mungkin perlu meneruskan argumen ke EditSiswaScreen
+                // itemId = backStackEntry.arguments?.getInt(itemIdArg) ?: -1
+            )
+        }
     }
 }
