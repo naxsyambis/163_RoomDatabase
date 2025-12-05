e5a66b240db3624f0564e7dbdf4b6dffdac8a6fb
naxsyambis <naxsyambis@gmail.com>
2025-12-05 16:29:54 +0700

    Menambahkan navigasi untuk melakukan update pada destinasi detail siswa

--- app/src/main/java/com/example/a163_roomdatabase/view/uicontroller/PetaNavigasi.kt
+++ app/src/main/java/com/example/a163_roomdatabase/view/uicontroller/PetaNavigasi.kt
@@ -4,11 +4,16 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
+import com.example.a163_roomdatabase.view.DetailSiswaScreen
 import com.example.a163_roomdatabase.view.EntrySiswaScreen
 import com.example.a163_roomdatabase.view.HomeScreen
+import com.example.a163_roomdatabase.view.route.DestinasiDetailSiswa
+import com.example.a163_roomdatabase.view.route.DestinasiDetailSiswa.itemIdArg
 import com.example.a163_roomdatabase.view.route.DestinasiEntry
 import com.example.a163_roomdatabase.view.route.DestinasiHome
 
@@ -36,6 +41,10 @@ fun HostNavigasi(
             HomeScreen(
                 navigateToItemEntry = {
                     navController.navigate(DestinasiEntry.route)
+                },
+
+                navigateToItemUpdate = { id ->
+                    navController.navigate("${DestinasiDetailSiswa.route}/$id")
                 }
             )
         }
@@ -45,5 +54,16 @@ fun HostNavigasi(
                 navigateBack = { navController.popBackStack() }
             )
         }
+
+        composable(route = DestinasiDetailSiswa.routeWithArgs,
+            arguments = listOf(navArgument(itemIdArg) {
+                type = NavType.IntType
+            })
+        ){
+            DetailSiswaScreen(
+                navigateBack = {navController.navigateUp()}
+            )
+        }
+
     }
 }
